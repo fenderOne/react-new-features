9307146ba1cc3e46d67cec64e3fcd05c44e85214
fenderOne <fenderOne@users.noreply.github.com>
2022-10-03 11:22:00 +0200

    counter erased

--- src/index.js
+++ src/index.js
@@ -62,30 +62,6 @@ const NoteApp = () => {
   );
 };
 
-const App = (props) => {
-  const [count, setCount] = useState(props.count);
-  const [text, setText] = useState('');
-
-  useEffect(() => {
-    console.log('This should only run once!');
-  }, []);
-
-  useEffect(() => {
-    console.log('useEffect ran');
-    document.title = count;
-  }, [count]);
-
-  return (
-    <div>
-      <p>The current {text || 'count'} is {count}</p>
-      <button onClick={() => setCount(count - 1)}>-1</button>
-      <button onClick={() => setCount(props.count)}>reset</button>
-      <button onClick={() => setCount(count + 1)}>+1</button>
-      <input value={text} onChange={(e) => setText(e.target.value)} />
-    </div>
-  );
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
